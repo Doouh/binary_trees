9bfa76451d7e60c76d1573e015d791f2d6a54b4e
Doouh <ake.giraldo@hotmail.com>
2020-03-09 15:03:39 -0500

    Add task 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - entry point
+ * @parent: pointer to the parent
+ * @value: value to save
+ *
+ * Return: binary tree
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t new_tree = NULL;
+
+	return (new_tree);
+}
