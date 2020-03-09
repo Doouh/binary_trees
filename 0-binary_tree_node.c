d653b017813b257e7b743c5a2495844abcb89672
Oscar Riano Tapias <oscarnetworkingpro@gmail.com>
2020-03-09 16:36:05 -0500

    Change

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,7 +10,25 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t new_tree = NULL;
+	binary_tree_t *new_tree;
+	binary_tree_t *current;
 
+	new_tree = malloc(sizeof(binary_tree_t));
+	if (new_tree == NULL)
+		return NULL;
+	new_tree->n = value;
+	new_tree->left = NULL;
+	new_tree->right = NULL;
+	new_tree->parent = NULL;
+	if(parent == NULL)
+		parent = new_tree;
+	else
+	{
+		current = parent;
+		parent = NULL;
+
+		while(current)
+	}
+	
 	return (new_tree);
 }
