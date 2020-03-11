8f3969f6c4214b9d62b59ef3dc0705977b0335c4
Oscar Riano Tapias <oscarnetworkingpro@gmail.com>
2020-03-11 14:49:38 -0500

    function that checks if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,6 @@
 #include "binary_trees.h"
+#include "9-binary_tree_height.c"
+#include "11-binary_tree_size.c"
 
 /**
  * binary_tree_is_perfect - function that checks if a binary tree is perfect
@@ -10,4 +12,18 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int height, max_nodes, size, i = 1, pow = 2;
+
+	if (!tree)
+		return (0);
+
+	height = (int)binary_tree_height(tree);
+	for ( ; i < height + 1; i++)
+		pow *= 2;
+	max_nodes = pow - 1;
+	size = binary_tree_size(tree);
+	if (size == max_nodes || height == 0)
+		return (1);
+	else
+		return (0);
 }
